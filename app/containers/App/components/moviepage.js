41525a5924ce45c2277de566b57ae64cd30a80fe
Ian Clark <ianclark226@gmail.com>
2021-04-22 14:41:27 -0400

    routing and moviepage

---
+++ app/containers/App/components/moviepage.js
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from 'react'
+import JsonData from '../../../public/movie-reviews.json';
+import ReactPaginate from 'react-paginate'
+
+
+
+
+
+
+
+
+import '../../../styles/styles.scss'
+import { get, includes } from 'lodash';
+
+
+function MoviePage() {
+ const [users, setUsers] = useState(JsonData.slice(0, 50));
+ const [pageNumber, setPageNumber] = useState([0]);
+
+const usersPerPage = 20;
+
+const pagesVisited = pageNumber * usersPerPage;
+
+
+
+const pageCount = Math.ceil(users.length / usersPerPage);
+
+const changePage = ({selected}) => {
+setPageNumber(selected)
+}
+
+  
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+
+
+//GET CURRENT POSTS
+
+  return (
+
+    <div className="App">
+      <header>
+          <h1> Critics</h1>
+      </header>
+     
+      <input 
+      type="text" 
+      placeholder="search critic" onChange={(e) => {setSearchTerm(e.target.value)}}
+      />
+      {JsonData
+      .filter((val) => {
+        if(searchTerm === "") {
+          return val;
+        } else if (val.display_name.toLowerCase().includes(searchTerm.toLowerCase())) {
+          return val;
+        }
+      })
+       
+      .slice(pagesVisited, pagesVisited + usersPerPage)
+
+      .map((val, key) => {
+        return (
+          <div className="user" key={key}>
+            <p>{val.display_title}</p>
+            <p>{val.mpaa_rating}</p>
+            <p>{val.critics_pick}</p>
+            <p>{val.byline}</p>
+            <p>{val.headline}</p>
+            <p>{val.summary_short}</p>
+            <p>{val.publication_date}</p>
+            <p>{val.opening_date}</p>
+            <p>{val.date_updated}</p>
+            </div>
+        )
+      })}
+
+      {/*  ADD PAGES FOR USERS */}
+
+     <ReactPaginate
+      previousLabel={"Previous"}
+      nextLabel={"Next"}
+      pageCount={pageCount}
+      onPageChange={changePage}
+      containerClassName={"paginationBttns"}
+      previousLinkClassName={"previousBttn"}
+      nextLinkClassName={"nextBttn"}
+      disabledClassName={"paginationDisabled"}
+      activeClassName={"paginationActive"}
+    /> 
+    
+    
+        {/* SUDO CODE FOR STUFF I DID NOT COMPLETE */}
+
+        {/* -IMPORT MOVIE REVIEWS THE SAME WAY I DID WITH CRITICS */}
+
+        {/* -MAKE AN ONCLICK EVENT FOR THE CRITIC THAT REVIEWED THE SPECIFIC MOVIE */}
+
+        {/* USE THE FILTER MORE FUNCTIONALBE WITH SPECIFIC DETAILS WITHIN THE REQUIREMENTS IN THE EXISTING CODE FOR NAME OF CRITIC */}
+
+    
+          </div>
+      )
+  }
+ 
+
+  export default MoviePage;
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -6,8 +6,11 @@
  * contain code that should be seen on all pages. (e.g. navigation bar)
  */
 
+import CriticPage from './components/criticpage';
+import MoviePage from './components/moviepage';
 import React, { useEffect, useState } from 'react'
-import JsonData from '../../public/critics.json'
+
+
 import ReactPaginate from 'react-paginate'
 
 
@@ -21,94 +24,28 @@ import HomePage from 'containers/HomePage/HomePage'
 
 import '../../styles/styles.scss'
 import { get, includes } from 'lodash';
+import { render } from 'react-testing-library';
 
 function App() {
- const [users, setUsers] = useState(JsonData.slice(0, 50));
- const [pageNumber, setPageNumber] = useState([0]);
-
-const usersPerPage = 20;
-
-const pagesVisited = pageNumber * usersPerPage;
-
-
-
-const pageCount = Math.ceil(users.length / usersPerPage);
-
-const changePage = ({selected}) => {
-setPageNumber(selected)
-}
-
-  
-
-  const [searchTerm, setSearchTerm] = useState('');
-
-
-
-//GET CURRENT POSTS
 
   return (
-
-    <div className="App">
-      <header>
-     
-      <input 
-      type="text" 
-      placeholder="search critic" onChange={(e) => {setSearchTerm(e.target.value)}}
-      />
-      {JsonData
-      .filter((val) => {
-        if(searchTerm === "") {
-          return val;
-        } else if (val.display_name.toLowerCase().includes(searchTerm.toLowerCase())) {
-          return val;
-        }
-      })
-       
-      .slice(pagesVisited, pagesVisited + usersPerPage)
-
-      .map((val, key) => {
-        return (
-          <div className="user" key={key}>
-            <p>{val.display_name}</p>
-            <p>{val.sort_name}</p>
-            <p>{val.status}</p>
-            <p>{val.bio}</p>
-            <p>{val.seo_name}</p>
-            </div>
-        )
-      })}
-
-      {/*  ADD PAGES FOR USERS */}
-
-     <ReactPaginate
-      previousLabel={"Previous"}
-      nextLabel={"Next"}
-      pageCount={pageCount}
-      onPageChange={changePage}
-      containerClassName={"paginationBttns"}
-      previousLinkClassName={"previousBttn"}
-      nextLinkClassName={"nextBttn"}
-      disabledClassName={"paginationDisabled"}
-      activeClassName={"paginationActive"}
-    /> 
-    </header>
-    <Switch>
-          <Route path="/" component={HomePage} exact />
-          <Route path="/home" component={HomePage} />
-        </Switch>
-
-        {/* SUDO CODE FOR STUFF I DID NOT COMPLETE */}
-
-        {/* -IMPORT MOVIE REVIEWS THE SAME WAY I DID WITH CRITICS */}
-
-        {/* -MAKE AN ONCLICK EVENT FOR THE CRITIC THAT REVIEWED THE SPECIFIC MOVIE */}
-
-        {/* USE THE FILTER MORE FUNCTIONALBE WITH SPECIFIC DETAILS WITHIN THE REQUIREMENTS IN THE EXISTING CODE FOR NAME OF CRITIC */}
-
     
-          </div>
-      )
-  }
- 
 
-  export default App;
+    <Route>
+      
+    <Switch>
+          {/* <Route path="/" component={HomePage} exact /> */}
+          <Route path="/" exact render={() => <CriticPage /> } />
+          <Route path="/moviepage" exact render={() => <MoviePage /> } />
+        </Switch>
+        </Route>
+        
+
+  )
+
+ 
+  
+       
+}
+
+export default App;
